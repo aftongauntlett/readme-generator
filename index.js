1a4cba40e29c53f84bb3b7082596e4521e7f0641
Afton Gauntlett <afton.gauntlett@gmail.com>
2020-04-23 16:29:57 -0400

    added questions

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 
 // -- They give us an ARRAY called 'questions' What could we do with this (?) -- //
-const questions = [
-
-];
+const questions = ["username" , "project" , "description" , "table" , "installation" ,"usage" ,"license","contributing" ,"test" ,"questions" ,"picture","email"];
 
 // -- They give us a writeToFile() FUNCTION, Looks like we may need to read/write to a file. What BUILT-IN node module will help us out with this (?) -- // 
 function writeToFile(fileName, data) {
@@ -15,3 +13,17 @@ function init() {
 
 // -- We DEFINED our INITALIZATION FUNCTION above, here we are just kicking off (running) our program. -- // 
 init();
+
+
+// Questions to ask: Through Inquirer
+
+// “What is your GitHub username?”
+// “What is your email?”
+// “the URL to your project?”
+// “What is your project’s name?”
+// “Please write a short description of your project”
+// “What kind of license should your project have?” - This one should be a list
+// “What command should be run to install dependencies?”
+// “What command should be run to run tests?”
+// “What does the user need to know about using the repo?”
+// “What does the user need to know about contributing to the repo?”
