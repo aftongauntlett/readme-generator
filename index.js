6469f28b893020b05e4a68d391e22ade4b4b75d1
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-04-25 19:27:36 -0400

    added question results to function, added badge

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+// start with my variables for libraries
 const inquirer = require("inquirer")
 const fs = require("fs")
 const axios = require("axios")
 
-// -- created an array of questions/objects -- //
+// -- create an array of questions/objects using const, since I don't want these to change -- //
 const questions = [{
     type: "input",
     message: "Enter your Github username",
@@ -37,7 +38,7 @@ const questions = [{
     type: "list",
     message: "What kind of license should your project have?",
     choices: ["MIT", "Apache 2.0", "GNU GPLv3", "Mozilla Public License 2.0"],
-    name: "table"
+    name: "license"
 },
 
 {
@@ -49,7 +50,7 @@ const questions = [{
 {
     type: "input",
     message: "What command should be run to run tests?",
-    name: "tests"
+    name: "test"
 },
 
 {
@@ -59,19 +60,37 @@ const questions = [{
 },
 ]
 
-// -- They give us a writeToFile() FUNCTION, Looks like we may need to read/write to a file. What BUILT-IN node module will help us out with this (?) -- // 
+// create writetoFile function and use fs to save to the generated readme file, save that to a folder I created called output.
 function writeToFile(fileName, data) {
-}
-
-// -- This is a fairly common programming construct. They are just giving us a FUNCTION to INITIALIZE or SETUP our project parameter. It's also where we usually kick off our project flow -- //
-
-function init() {
-    inquirer.prompt(questions).then(function (userInputObject) {
-        console.log(userInputObject, userInputObject.username)
+    fs.writeFile("output/" + fileName, data, function (err) {
+        if (err) return console.log(err);
+        console.log('Hello World > helloworld.txt');
     })
 }
 
-// -- We DEFINED our INITALIZATION FUNCTION above, here we are just kicking off (running) our program. -- // 
+// Create a function to run through all questions and output them into a readme
+//  my backtick button is broken on my desktop, that is why I am concatinating. Will work on fixing that soon)
+function init() {
+    inquirer.prompt(questions).then(function (userInputObject) {
+        console.log(userInputObject, userInputObject.username)
+        let username = userInputObject.username
+        let output = "# " + userInputObject.project
+        output += "\n\n" + `![GitHub followers](https://img.shields.io/github/followers/${username}?style=social)`
+        output += "\n\nDeveloped by: " + userInputObject.username
+        output += "\n\nContact: " + userInputObject.email
+        output += "\n\nproject: " + userInputObject.project
+        output += "\n\nprojectURL: " + userInputObject.projectURL
+        output += "\n\nDescription: " + userInputObject.discription
+        output += "\n\nDescription: " + userInputObject.discription
+        output += "\n\nLicenses: " + userInputObject.license
+        output += "\n\nInstallation information: " + userInputObject.installation
+        output += "\n\nHow to run a test: " + userInputObject.test
+        output += "\n\nInformation about the repo: " + userInputObject.repo
+        writeToFile("readme.md", output)
+    })
+}
+
+// run the function init
 init();
 
 
---
+++ output/readme.md
@@ -0,0 +1,23 @@
+# sf
+
+![GitHub followers](https://img.shields.io/github/followers/aftongauntlett?style=social)
+
+Developed by: aftongauntlett
+
+Contact: sf
+
+project: sf
+
+projectURL: sf
+
+Description: undefined
+
+Description: undefined
+
+Licenses: MIT
+
+Installation information: sf
+
+How to run a test: sf
+
+Information about the repo: sf
